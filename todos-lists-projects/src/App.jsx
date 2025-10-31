f3e59dabd79b8387f310e73852d2020277af6ea3
darunbjork <darunbjork@gmail.com>
2025-10-31 19:54:47 +0100

    Connect the Input Field

--- todos-lists-projects/src/App.jsx
+++ todos-lists-projects/src/App.jsx
@@ -2,15 +2,23 @@ import { useState } from "react";
 
 function App() {
   const [tasks, setTasks] = useState([]);
-
   const [newTaskText, setNewTaskText] = useState("");
 
   return (
     <div style={{ padding: "20px" }}>
       <h1>My To-Do List</h1>
 
-      <p>Number of tasks: {tasks.length}</p>
-      <p>Current input text: {newTaskText}</p>
+      <div>
+        <input
+          type="text"
+          placeholder="What needs to be done?"
+          value={newTaskText}
+          onChange={(event) => setNewTaskText(event.target.value)}
+        />
+        <button>Add Task</button>
+      </div>
+      <hr />
+      <p>Current input text: **"{newTaskText}"**</p>
     </div>
   );
 }
