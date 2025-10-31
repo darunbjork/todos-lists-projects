43d54015bb4fd1246adcf920ce29bb363d2d04ac
darunbjork <darunbjork@gmail.com>
2025-10-31 20:03:09 +0100

    Adding Tasks and Updating Array State

--- todos-lists-projects/src/App.jsx
+++ todos-lists-projects/src/App.jsx
@@ -4,6 +4,23 @@ function App() {
   const [tasks, setTasks] = useState([]);
   const [newTaskText, setNewTaskText] = useState("");
 
+  const handleAddTask = () => {
+    if (newTaskText.trim() === "") {
+      return;
+    }
+
+    const newId = Date.now();
+    const newTask = {
+      id: newId,
+      text: newTaskText,
+      isComplete: false,
+    };
+
+    setTasks([...tasks, newTask]);
+
+    setNewTaskText("");
+  };
+
   return (
     <div style={{ padding: "20px" }}>
       <h1>My To-Do List</h1>
@@ -15,10 +32,14 @@ function App() {
           value={newTaskText}
           onChange={(event) => setNewTaskText(event.target.value)}
         />
-        <button>Add Task</button>
+
+        <button onClick={handleAddTask}>Add Task</button>
       </div>
+
       <hr />
-      <p>Current input text: **"{newTaskText}"**</p>
+
+      <p>Number of tasks: **{tasks.length}**</p>
+      <p>Current text: {newTaskText}</p>
     </div>
   );
 }
